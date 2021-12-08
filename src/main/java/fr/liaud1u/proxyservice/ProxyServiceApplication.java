7f548deab31ff339a1be2da43f89ce42da2649af
liaud1u <alexis.liaud7@etu.univ-lorraine.fr>
2021-12-08 19:16:37 +0100

    Initial commit

---
+++ src/main/java/fr/liaud1u/proxyservice/ProxyServiceApplication.java
@@ -0,0 +1,15 @@
+package fr.liaud1u.proxyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ProxyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyServiceApplication.class, args);
+    }
+
+}
